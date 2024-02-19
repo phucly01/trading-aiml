eb622a1a4f0f8975efc881d108bc510e74ec70a8
phucly01 <phongthuypts@yahoo.com>
2024-02-19 10:41:18 -0500

    Fixed the script to support both local folder and FS mounted folder.  For local folder, venv can use symbolic links.  For FS mounted folder, copies of all required packages will be in the env folder.

--- runMeFirst.sh
+++ runMeFirst.sh
@@ -4,12 +4,36 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd ${SCRIPT_DIR}
 
-python -m venv ./env
+echo "Creating Virtual Environment"
+
+if [ -z "`pip list | grep virtualenv`" ]; then
+    #Install virtualenv instead of using python -m env.  Virtualenv handles vboxsf better.
+    echo "Installing virtualenv module"
+    pip install virtualenv
+fi
+
+#python -m venv --copies ./env
+virtualenv ./env   #Try default options via symbolic links first
+if [ "$?" -ne 0 ]; then
+    echo "Symbolic links isn't allowed.  Trying copies"
+    virtualenv --always-copy ./env
+fi
 
 if [ -d ${SCRIPT_DIR}/env/Scripts ]; then
-   pushd ${SCRIPT_DIR}/env/Scripts
-   ./activate
-
-   if [ -f ${SCRIPT_DIR}/requirements.txt ]; then
-        pip install -r ${SCRIPT_DIR}/requirements.txt
+    echo "Activating Virtual Environment"
+    pushd ${SCRIPT_DIR}/env/Scripts
+    ./activate
+    popd
+elif [ -d ${SCRIPT_DIR}/env/bin ]; then
+    echo "Activating Virtual Environment"
+    pushd ${SCRIPT_DIR}/env/bin
+    ./activate
+    popd
 fi
+
+#requirements.txt can be created via pipreqs
+#Update the version packages in requirements.txt via pip install -U -r requirements.txt
+if [ -f ${SCRIPT_DIR}/requirements.txt ]; then
+        echo "Installing required packages from requirements.txt"
+        pip install -r ${SCRIPT_DIR}/requirements.txt
+fi 
